e8aa88a1e498681faf4a72d606d4638532d7d8d8
ccb-590 <born2fish@yandex.ru>
2017-02-20 17:25:14 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+  name='mr_csv_worker',
+  packages=['mr_csv_worker'],
+  version='0.0.1',
+  description='Small package for assist telegram bot development',
+  author='Root Kid',
+  author_email='shaman@born2fish.ru',
+  url='https://github.com/r00tkid/mr_csv_worker.git',
+  download_url='https://github.com/r00tkid/mr_csv_worker/tarball/0.0.1',
+  keywords=['support', 'csv', 'read csv', 'write csv'],  # keywords
+  classifiers=[],
+)
+
